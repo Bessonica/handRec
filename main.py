186d56bbf220ff8b1aed472b390aa7f46518e3fb
Bessonica <43250127+Bessonica@users.noreply.github.com>
2020-12-21 11:28:22 +0200

    Update main.py
    модель медленная,все еще нет поддержки веб камеры

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import random
 dir_train = "D:/programming/deeplearning/dataset/hands/train"
 dir_test = "D:/programming/deeplearning/dataset/hands/test"
 #можно лиD:/programming/deeplearning/dataset/hands/train/*.png???7!!!
-
+# что такоке glob?
 labels = ["0L", "1L", "2L", "3L", "4L", "5L", "0R", "1R", "2R", "3R", "4R", "5R", ]
 
 #создаем массивы изображений и ярлыков,которые обозначают руку и кол пальцев
@@ -23,7 +23,6 @@ arr_test = []
 #listdir дает имена файлов  img_train = str_train
 #не забывай что у имени в конце есть еще .png. -1 = идем справо налево
 
-#arr_train
 for str_train in os.listdir(dir_train):
     str_label = str_train[-5: -7: -1][::-1]
     label = labels.index(str_label)
@@ -45,6 +44,10 @@ for str_test in os.listdir(dir_test):
 
 
 
+
+
+#reshape и сделать черно белім изображение
+
 #проверяем датасет
 #print(arr_test[0][0])
 #print(labels[arr_test[0][1]])
@@ -57,7 +60,7 @@ for str_test in os.listdir(dir_test):
 
 
 
-#отделяем фото от меток
+#отделяем массивы фото от меток
 
 arr_train_img = []
 arr_train_label = []
@@ -89,25 +92,21 @@ for img_test, label_test in arr_test:
 # print(labels[arr_test_label[0]])
 
 
+arr_train_img = np.array(arr_train_img).reshape(-1, 128, 128, 1)
 
-#print(arr_test_img.shape)#'list' object has no attribute 'shape'.решение- использовать np
+#arr_test_img это лист (list) из массивов,в каждом массиве 128 масивов с 128 элементов????
 
-#arr_test_img    это лист наполненный массивами.превращаем этот лист в один массив и делаем изображение черно белым
-arr_test_img = np.array(arr_test_img)
+#!!что если сделать цикл,где ті проходишь arr_test_img[i]( и делишь их на 255.0!!
+
+
+
+arr_test_img = np.array(arr_test_img).reshape(-1, 128, 128, 1)
+#print(arr_test_img[0].shape)#128,128 до ришейпа.   после (128, 128, 1)
+#осле ришейпа 1 изображение  это массив из масивов в которых числа(пиксели)в отдельных
+#массивах  [ [[15]...[15](128 элеиентов) ](строка 1) ... [[0]...(128 элеиентов)[0]](строка 128)  ]
+
 arr_test_img = arr_test_img / 255.0
-
-#print(arr_test_img) #.shape = 3600, 128, 128 заметь что команда почти точно такаяже как и ришейп ниже
-#можно ли теперь делить?
-
-
-
-#print(arr_test_img)
-#найс,вроде все норм поделилось,!!!!но протестируй NN на тестовом массиве
-
-#arr_train_img
-arr_train_img = np.array(arr_train_img)
 arr_train_img = arr_train_img / 255.0
-print(arr_train_img.shape)#.shape 18000, 128, 128
 
 
 arr_train_label = np.array(arr_train_label)
@@ -123,37 +122,51 @@ arr_test_label = np.array(arr_test_label)
 
 
 #создаем модель
+#в ANN можно было скормить массивы без ришейпа
+#но с CNN необходимо изменить лист массивов,что бы скормить моделе.
+#иначе будет ошибка  input 0 of layer sequential is incompatible with the layer expected min_ndim=4 found ndim=3
+#
+#print("shape img train", arr_train_img.shape[1:]) #128, 128, 1
 
-#сколько нейронов выбрать?прочитал что оптимальный выбор числа нейронов между
-#инпутом(128*128 = 16 384) и аутпутом(12) многовато,нужно тестить
-#начну с 400+может быть сделать 2 внутренних слоя
-#точность 0.99 так что трех слоев достаточно.!!!не забудь протестировать на новой инфе(массив test и
-# еще есть папка fingers)
+
+#!!!!!!!!!!!!!!!!!!!    ЭКСПЕРЕМЕНТИРУЮ С МОДЕЛЬЮ      !!!!!!!!!!!!
+#модель неверотно массивная(параметров больше ляма)  conv2d(20) еще 40 размер=3, 3
+
+#уменьшил кол функций conv2d(20,(10, 10)) и увеличил их размеры(10,10) параметров меньше 184тыс,но невероятно медленна
+
+#уменьшил кол функций conv2d(20) и уменьшил их размеры(3,3)
+#модель работает лучше,но все еще невероятно медленно,но модель быстрее учится.ей достаточно 2 epochs что бы быть +-100% точным
+
+#попробую увеличить пулинг,было 2, 2.сделаю 5, 5
+#модель учится намного быстрее, но меня все еще раздражает что моя CNN работает медленее ANN,хотя в теории должно быть
+#наоборот(параметров 9тыс)
+
+#сделаю пул 10, 10.Появилась ошибка на строке где последний conv2d - Value error negative dimension size caused by subtracting 3 from 1 for ...
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(20, (3, 3), activation="relu", input_shape=(128, 128, 1)))
+model.add(tf.keras.layers.MaxPool2D((5, 5)))
+model.add(tf.keras.layers.Conv2D(20, (3, 3), activation="relu"))
+model.add(tf.keras.layers.MaxPool2D((5, 5)))
+model.add(tf.keras.layers.Conv2D(20, (3, 3), activation="relu"))
+model.summary()
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(12))
+model.summary()
+
+model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+model.fit(arr_train_img, arr_train_label, batch_size=32, epochs=2)
 
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(128, 128)),
-    tf.keras.layers.Dense(400, activation="relu"),
-    tf.keras.layers.Dense(12, activation="softmax")
-])
-#разобраться в разнице между разными optimizer что делает RMSprop
-model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-#batc size по дефолту они и так 32,но у нас датасет и нужно обязательно указать.по крайней мере так прочитал
-#в документации
-#вспомни в чем разница между .fit и .evaluate
-model.fit(arr_train_img, arr_train_label,batch_size=32, epochs=5)
-
-#тистируем
 test_loss, test_acc = model.evaluate(arr_test_img, arr_test_label)
+print("test acc", test_acc)  #0.99
 
-print("Окончательная точность", test_acc)# 0.995
-
-#выводы 19.12.20
-#медленный,можно оптимизировать?
-#сделать CNN и добавить поддержку вебкамеры
-
-
-
-
+#21.12.2020 выводы
+#CNN готова но я не доволен ее скоростью,почитать способы оптимизации
+#все еще нет поддержки веб камеры,попытайся не использовать object detection api от тенсерфлоу,а просто сделать все используя только cv2
+#ОТДЕЛИ МОДЕЛЬ ОТ ОСНОВНОЙ ЧАСТИ КОДА,что бы не создавать и учить модель каждый раз когда запускаешь код
+#ПОСТАВЬ ГИТ В pycharm
 
 
