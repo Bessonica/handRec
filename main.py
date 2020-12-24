ef23697bcfa7a102b76f766ae2e4e2fbea36ca8f
Bessonica <43250127+Bessonica@users.noreply.github.com>
2020-12-24 21:35:52 +0200

    убрал комментарии
    все еще не могу обьединить вебку и нс.попробуй начать сначала работу с камерами

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ dir_test = "D:/programming/deeplearning/dataset/hands/test"
 # что такоке glob?
 labels = ["0L", "1L", "2L", "3L", "4L", "5L", "0R", "1R", "2R", "3R", "4R", "5R", ]
 
-#создаем массивы изображений и ярлыков,которые обозначают руку и кол пальцев
+#создаем list изображений и ярлыков,которые обозначают руку и кол пальцев
 
 
 arr_train = []
@@ -46,18 +46,6 @@ for str_test in os.listdir(dir_test):
 
 
 
-#reshape и сделать черно белім изображение
-
-#проверяем датасет
-#print(arr_test[0][0])
-#print(labels[arr_test[0][1]])
-#print(len(arr_train))#18000
-#print(arr_train[0][0].shape)#128,128
-#print(arr_train[0].shape)#'list' object has no attribute 'shape'
-#print(arr_train.shape) 18000, 2
-
-
-
 
 
 #отделяем фото от меток
@@ -73,34 +61,15 @@ for img_train, label_train in arr_train:
     arr_train_label.append(label_train)
 
 
-# #проверка
-# print("img : ", len(arr_train_img)) #18000
-# print("label : ", len(arr_train_label))  #18000
-# plt.imshow(arr_train_img[0], cmap = 'gray')
-# plt.show()
-# print(labels[arr_train_label[0]])
 
 for img_test, label_test in arr_test:
     arr_test_img.append(img_test)
     arr_test_label.append(label_test)
 
-# #проверка
-# print("img : ", len(arr_test_img)) #3600
-# print("label : ", len(arr_test_label))  #3600
-# plt.imshow(arr_test_img[0], cmap = 'gray')
-# plt.show()
-# print(labels[arr_test_label[0]])
-
-#!!!!!!!!!!!СТАРАЯ ВЕРСИЯ
-#arr_train_img = np.array(arr_train_img).reshape(-1, 128, 128, 1)
-
-#arr_test_img это лист (list) из массивов,в каждом массиве 128 масивов с 128 элементов????
-
-#!!что если сделать цикл,где ті проходишь arr_test_img[i]( и делишь их на 255.0!!
 
 
 #НОВАЯ ВЕРСИЯ
-#print(arr_test_img.shape)#'list' object has no attribute 'shape'.решение- использовать np
+#print(arr_test_img.shape)#'list' object has no attribute 'shape'.решение= использовать np
 arr_test_img = np.array(arr_test_img)
 #print(arr_test_img) #.shape = 3600, 128, 128 заметь что команда почти точно такаяже как и ришейп ниже
 #можно ли теперь делить?
@@ -113,15 +82,7 @@ print(arr_train_img)
 arr_train_img = arr_train_img / 255.0
 print(arr_train_img)
 print(arr_train_img.shape)#.shape 18000, 128, 128
-#!!!!!!!!!!СТАРАЯ ВЕРСИЯ
-#arr_test_img = np.array(arr_test_img).reshape(-1, 128, 128, 1)
 
-#print(arr_test_img[0].shape)#128,128 до ришейпа.   после (128, 128, 1)
-#осле ришейпа 1 изображение  это массив из масивов в которых числа(пиксели)в отдельных
-#массивах  [ [[15]...[15](128 элеиентов) ](строка 1) ... [[0]...(128 элеиентов)[0]](строка 128)  ]
-
-#arr_test_img = arr_test_img / 255.0
-#arr_train_img = arr_train_img / 255.0
 
 
 arr_train_label = np.array(arr_train_label)
@@ -136,22 +97,10 @@ arr_test_label = np.array(arr_test_label)
 # print("test label", len(arr_test_label))
 
 
-#plt.imshow(arr_test_img[10])#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!test
-
-
-# plt.grid(False)
-# plt.imshow(arr_test_img[10], cmap=plt.cm.binary)
-# plt.xlabel(labels[arr_test_label[10]]) #value error: dictionary update sequence element #0 has length 1; 2 is required
-# print(labels[arr_test_label[10]])
-# plt.show()
-
-
 
 
 #создаем модель
 #print("shape img train", arr_train_img.shape[1:]) #128, 128, 1
-
-#до ришейпа это лист массивов что подходит для инпут дата,проверить.но как сделать пиксели черно белыми?
 
 #сколько нейронов выбрать?прочитал что оптимальный выбор числа нейронов между
 #инпутом(128*128 = 16 384) и аутпутом(12) многовато,нужно тестить
@@ -159,7 +108,6 @@ arr_test_label = np.array(arr_test_label)
 #точность 0.99 так что трех слоев достаточно.!!!не забудь протестировать на новой инфе(массив test и
 # еще есть папка fingers)
 
-#input_shape был (128, 128, 1) когда был ришейп
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(128, 128)),
     tf.keras.layers.Dense(400, activation="relu"),
@@ -169,7 +117,7 @@ model = tf.keras.Sequential([
 model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 #batc size по дефолту они и так 32,но у нас датасет и нужно обязательно указать.по крайней мере так прочитал
 #в документации
-#вспомни в чем разница между .fit и .evaluate и зачем указывать в fit тест массивы
+
 model.fit(arr_train_img, arr_train_label,batch_size=32, epochs=3)  #3 epochs достаточно
 
 
